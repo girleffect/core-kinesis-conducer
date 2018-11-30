0a39963f35f5bd5e973fba8b5ef7b501b36bbf59
AltusBarry <AltusBarry@users.noreply.github.com>
2018-11-30 16:12:53 +0200

    feature/GEINFRA-360: Added producer and requirements

---
+++ kinesis_conducer/producer/__init__.py
@@ -0,0 +1,39 @@
+import os
+
+import boto3
+from environs import Env
+
+from kinesis_conducer.producers import GEKinesisProducer
+
+env = Env()
+
+# TODO generalise comment
+# To ensure there are no Django startup issues, this module does not rely
+# on settings.py for setup variables.
+# It can also not be instantiated under all circumstances. The extra process
+# the producer creates halts some of Django's management commands, as the
+# process does not terminate automatically.
+if env.bool("USE_KINESIS_PRODUCER", False) and not env.bool("BUILDER", False):
+    print("Kinesis producer setup commencing...")
+
+    print("Creating Kinesis session...")
+    # Boto3 session that will be used for authentication
+    KINESIS_SESSION = boto3.Session(**env.dict("KINESIS_SESSION"))
+
+    print("Creating producer settings...")
+    # The AsyncProducer that will be used to perform put_records
+    PRODUCER_SETTINGS = env.dict("KINESIS_PRODUCER")
+    print("Setting producer session...")
+    PRODUCER_SETTINGS["boto3_session"] = KINESIS_SESSION
+
+    print("Setting client settings...")
+    # Override the boto3 client settings, if needed
+    CLIENT_SETTINGS = env.dict("KINESIS_BOTO3_CLIENT_SETTINGS") \
+        if "KINESIS_BOTO3_CLIENT_SETTINGS" in os.environ else dict()
+    if CLIENT_SETTINGS.get("endpoint_url"):
+        print("Setting producer client settings...")
+        PRODUCER_SETTINGS["boto3_client_settings"] = CLIENT_SETTINGS
+
+    print("Creating producer...")
+    KINESIS_PRODUCER = GEKinesisProducer(**PRODUCER_SETTINGS)
+    print("Kinesis producer created!")
---
+++ kinesis_conducer/producer_events/events.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+import json
+
+from kinesis_conducer.producer_events import schemas, utils
+from kinesis_conducer.producer import KINESIS_PRODUCER
+
+
+def put_event(event_type, site_id, **kwargs):
+    """
+    Generic method used for most Kinesis put record events.
+    """
+
+    # TODO: Check if this needs more elegant handling.
+    schema = schemas.EventTypes.SCHEMAS[event_type]
+
+    # Append BASE_SCHEMA values to all event data
+    kwargs.update(
+        {
+            "timestamp": datetime.now().isoformat(),
+            "site_id": site_id,
+            "event_type": event_type
+        }
+    )
+    utils.validate(kwargs, schema)
+
+    # Base producer supports only the singular put event, it does a
+    # boto3.client.put_records of all queued events.
+    KINESIS_PRODUCER.put(json.dumps(kwargs), partition_key=event_type)
