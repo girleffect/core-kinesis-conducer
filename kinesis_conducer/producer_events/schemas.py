45750984d65485743c7cdc0726a37f05c0a5deaf
AltusBarry <AltusBarry@users.noreply.github.com>
2018-12-12 09:25:32 +0200

    feature/GEINFRA-382: Added new event schemas

--- kinesis_conducer/producer_events/schemas.py
+++ kinesis_conducer/producer_events/schemas.py
@@ -25,6 +25,11 @@ BASE_SCHEMA = {
 class EventTypes:
     USER_LOGIN = "USER_LOGIN"
     USER_LOGOUT = "USER_LOGOUT"
+    INVITATION_SENT = "INVITATION_SENT"
+    INVITATION_REDEEMED = "INVITATION_REDEEMED"
+    USER_DELETION_REQUESTED = "USER_DELETION_REQUESTED"
+    EXPIRED_INVITATION_PURGE_REQUESTED = "EXPIRED_INVITATION_PURGE_REQUESTED"
+    RESOURCE_CRUD = "RESOURCE_CRUD"
 
     SCHEMAS = {
         USER_LOGIN: {
@@ -80,6 +85,204 @@ class EventTypes:
                 "site_id",
                 "user_id"
             ],
+        },
+        INVITATION_SENT: {
+            "type": "object",
+            "additionalProperties": False,
+            "properties": {
+                "event_type": {
+                    "type": "string",
+                    "pattern": INVITATION_SENT
+                },
+                "timestamp": {
+                    "type": "string",
+                    "format": "date-time"
+                },
+                "site_id": {
+                    "type": "integer",
+                    "minimum": 0
+                },
+                "invitation_id": {
+                    "type": "string",
+                    "format": "uuid"
+                },
+                "invitor_id": {
+                    "type": "string",
+                    "format": "uuid",
+                    "description": "The user that created the invitation"
+                },
+                "recipient_email": {
+                    "type": "string",
+                    "format": "email"
+                },
+                "organisation_id": {
+                    "type": "integer"
+                }
+            },
+            "required": [
+                "event_type",
+                "timestamp",
+                "site_id",
+                "invitation_id",
+                "invitor_id",
+                "recipient_email",
+                "organisation_id"
+            ]
+        },
+        INVITATION_REDEEMED: {
+            "type": "object",
+            "additionalProperties": False,
+            "properties": {
+                "event_type": {
+                    "type": "string",
+                    "pattern": INVITATION_REDEEMED
+                },
+                "timestamp": {
+                    "type": "string",
+                    "format": "date-time"
+                },
+                "site_id": {
+                    "type": "integer",
+                    "minimum": 0
+                },
+                "invitation_id": {
+                    "type": "string",
+                    "format": "uuid"
+                },
+                "user_id": {
+                    "type": "string",
+                    "format": "uuid",
+                    "description": "The user that redeemed the invitation"
+                }
+            },
+            "required": [
+                "event_type",
+                "timestamp",
+                "site_id",
+                "invitation_id",
+                "user_id"
+            ],
+        },
+        USER_DELETION_REQUESTED: {
+            "type": "object",
+            "additionalProperties": False,
+            "properties": {
+                "event_type": {
+                    "type": "string",
+                    "pattern": "USER_DELETION_REQUESTED"
+                },
+                "timestamp": {
+                    "type": "string",
+                    "format": "date-time"
+                },
+                "site_id": {
+                    "type": "integer",
+                    "minimum": 0
+                },
+                "requestor_id": {
+                    "type": "string",
+                    "format": "uuid",
+                    "description": "The user that requested the deletion"
+                },
+                "user_id": {
+                    "type": "string",
+                    "format": "uuid",
+                    "description": "The user to be deleted"
+                },
+                "reason": {
+                    "type": "string",
+                }
+            },
+            "required": [
+                "event_type",
+                "timestamp",
+                "site_id",
+                "requestor_id",
+                "user_id",
+                "reason"
+            ]
+        },
+        EXPIRED_INVITATION_PURGE_REQUESTED: {
+            "type": "object",
+            "additionalProperties": False,
+            "properties": {
+                "event_type": {
+                    "type": "string",
+                    "pattern": EXPIRED_INVITATION_PURGE_REQUESTED
+                },
+                "timestamp": {
+                    "type": "string",
+                    "format": "date-time"
+                },
+                "site_id": {
+                    "type": "integer",
+                    "minimum": 0
+                },
+                "user_id": {
+                    "type": "string",
+                    "format": "uuid",
+                    "description": "The user that requested the purge"
+                },
+                "cutoff_date": {
+                    "type": "string",
+                    "format": "date",
+                    "description": "Optional cut-off date specified"
+                }
+            },
+            "required": [
+                "event_type",
+                "timestamp",
+                "site_id",
+                "user_id"
+            ]
+        },
+        RESOURCE_CRUD: {
+            "type": "object",
+            "additionalProperties": False,
+            "properties": {
+                "event_type": {
+                    "type": "string",
+                    "pattern": RESOURCE_CRUD
+                },
+                "timestamp": {
+                    "type": "string",
+                    "format": "date-time"
+                },
+                "site_id": {
+                    "type": "integer",
+                    "minimum": 0
+                },
+                "resource_urn": {
+                    "type": "string",
+                    "format": "urn"
+                },
+                "resource_id": {
+                  "type": "string",
+                  "description": "The id of the resource converted to a string",
+                },
+                "action": {
+                    "type": "string",
+                    "enum": ["create", "update", "delete"]
+                },
+                "user_id": {
+                    "type": "string",
+                    "format": "uuid",
+                },
+                "post_action_data": {
+                    "type": "object",
+                    "description": "Opaque object representing the state of the object"
+                }
+            },
+            "required": [
+                "event_type",
+                "timestamp",
+                "site_id",
+                "resource_urn",
+                "resource_id",
+                "action",
+                "user_id",
+                "post_action_data"
+            ]
         }
     }
 
